0d36f509c5fe10047a214744deacc5845700ead4
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2010-11-16 18:25:18 +0000

    изменил линки на google

--- trunk/sostav/peflsostav.user.js
+++ trunk/sostav/peflsostav.user.js
@@ -4,6 +4,8 @@
 // @description    Display sostav
 // @include        http://pefl.ru/pfs.php?sostav
 // @include        http://www.pefl.ru/pfs.php?sostav
+// @include        http://pefl.net/pfs.php?sostav
+// @include        http://www.pefl.net/pfs.php?sostav
 // ==/UserScript==
 
 var headID = document.getElementsByTagName("head")[0];         
@@ -13,5 +15,5 @@ newScript1.src = 'js/jquery-1.3.2.min.js';
 headID.appendChild(newScript1);
 var newScript2 = document.createElement('script');
 newScript2.type = 'text/javascript';
-newScript2.src = 'http://const.fanstvo.com/monkey/sostav.js';
+newScript2.src = 'http://pefladdons.googlecode.com/svn/trunk/sostav/sostav.js';
 headID.appendChild(newScript2);
