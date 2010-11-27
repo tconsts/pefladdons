75f6ed91ff218432d544bdff54e45787b6f2ffa7
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2010-11-27 18:18:12 +0000

    add second card

--- trunk/dev/firefox/sostav_na_match/main.js
+++ trunk/dev/firefox/sostav_na_match/main.js
@@ -60,7 +60,7 @@ function printCard (plid,type){
 	var playerpasses = pl["passes"]
 	var cardhtml = '[td valign=top width=20% bgcolor=#C9F8B7][table width=100% bgcolor=#A3DE8F]';
 
-	cardhtml += '[tr][td colspan=2][b]' + pl["firstname"][0] + '.' + (pl["secondname"]).replace(/\s/g,'').replace(/-/g,'') + '[/b][/td][/tr]';
+	cardhtml += '[tr][td colspan=2][b]' + (pl["firstname"] ? pl["firstname"][0] + '.' : '') + (pl["secondname"]).replace(/\s/g,'').replace(/-/g,'') + '[/b][/td][/tr]';
 	cardhtml += '[tr][td][player=' + plid + '][img]';
 	if (type == 0){
 		cardhtml += (pl["position"] == "GK" ? goalkeeper_player_img_src : field_player_img_src)
@@ -68,7 +68,7 @@ function printCard (plid,type){
 		cardhtml += (type == 1 ? goalkeeper_player_img_src : field_player_img_src)
 	}
 	cardhtml += '[/img][/player][/td]';
-	cardhtml += '[td valign=top height=41 ]';
+	cardhtml += '[td valign=top height=41]';
 	if (playergames != 0) {
 		cardhtml += 'P ' + pl["ratingav"] + '\n';
 		cardhtml += 'И ' + playergames;
@@ -78,7 +78,40 @@ function printCard (plid,type){
 		cardhtml += ' ';
 	}
 	cardhtml += '[/td][/tr]';
-	cardhtml += '[tr][td colspan=2 align=right]фрм' + pl["form"] + '/мрл' + pl["morale"] + '[/td][/tr]';
+	cardhtml += '[tr][td]фрм' + pl["form"] + '[/td][td]мрл' + pl["morale"] + '[/td][/tr]';
+	if (type == 0) {
+		cardhtml += '[tr][td colspan=2 align=center bgcolor=#C9F8B7]' + pl["position"] + '[/td][/tr]';
+	}
+	cardhtml += '[/table][/td]';
+	return cardhtml;
+}
+
+function printCard2 (plid,type){
+	var pl = players[plid];
+	var playergames = pl["games"]
+	var playermom = pl["mom"]
+	var playergoals = pl["goals"]
+	var playerpasses = pl["passes"]
+	var playervalue = pl["value"]
+	var cardhtml = '[td valign=top width=20% bgcolor=#C9F8B7][table width=100% height=100% bgcolor=#A3DE8F]';
+
+	cardhtml += '[tr][td colspan=2][b]' + (pl["firstname"] ? pl["firstname"][0] + '.' : '') + (pl["secondname"]).replace(/\s/g,'').replace(/-/g,'') + '[/b][/td][/tr]';
+	cardhtml += '[tr][td][player=' + plid + '][img]';
+	if (type == 0){
+		cardhtml += (pl["position"] == "GK" ? goalkeeper_player_img_src : field_player_img_src)
+	} else {
+		cardhtml += (type == 1 ? goalkeeper_player_img_src : field_player_img_src)
+	}
+	cardhtml += '[/img][/player][/td]';
+	cardhtml += '[td valign=top height=50 rowspan=2]';
+	cardhtml += pl["form"] + '/' + pl["morale"];
+	if (playergames != 0) {
+		cardhtml += '\nP ' + pl["ratingav"];
+		cardhtml += '\nИ ' + playergames + (playermom != 0 ? '(' + playermom + ')' : '');
+		cardhtml += (playergoals != 0 || playerpasses != 0 ? '\nГП/' + playergoals + '+' + playerpasses : '');
+	}
+	cardhtml += '[/td][/tr]';
+	cardhtml += '[tr][td align=center]' + (playervalue >= 1000 ? (playervalue/1000).toFixed(1) + 'м' : playervalue + 'т') + '$[/td][/tr]';
 	if (type == 0) {
 		cardhtml += '[tr][td colspan=2 align=center bgcolor=#C9F8B7]' + pl["position"] + '[/td][/tr]';
 	}
@@ -96,19 +129,24 @@ function fillTextarea(pt) {
 		preparedhtml += '[tr]';
 		
 		// 1
-		preparedhtml += '[td width=20% height=50] [/td]';
+		preparedhtml += '[td width=20%] [/td]';
 		
 		for(j=0;j<3;j++) {
-			if (sostav[23+j]) {
-				preparedhtml += ( pt==2 ? printCard(sostav[23+j],23+j) : printClassic(sostav[23+j],23+j));
+			var num = 23+j
+			if (sostav[num]) {
+				switch(pt) {
+					case 3: preparedhtml += printCard2(sostav[num],num);break;
+					case 2: preparedhtml += printCard(sostav[num],num);break;
+					default: printClassic(sostav[num],num)
+				}
 			} else {
-				preparedhtml += '[td width=20% height=50] [/td]';
+				preparedhtml += '[td width=20%] [/td]';
 			}
 
 		}
 		
 		// 5
-		preparedhtml += '[td width=20% height=50] [/td]';
+		preparedhtml += '[td width=20%] [/td]';
 		preparedhtml += '[/tr]';
 		
 		// для // AM // MF // DM // DF
@@ -116,8 +154,13 @@ function fillTextarea(pt) {
 			preparedhtml += '[tr]';
 			
 			for(j=0;j<5;j++) {
-				if (sostav[18-k+j]) {
-					preparedhtml += (pt==2 ? printCard(sostav[18-k+j],18-k+j) : printClassic(sostav[18-k+j],18-k+j))
+				var num = 18-k+j
+				if (sostav[num]) {
+					switch(pt) {
+						case 3: preparedhtml += printCard2(sostav[num],num);break;
+						case 2: preparedhtml += printCard(sostav[num],num);break;
+						default: printClassic(sostav[num],num)
+					}
 				} else {
 					preparedhtml += '[td width=20% height=50] [/td]';
 				}
@@ -130,27 +173,20 @@ function fillTextarea(pt) {
 		
 			preparedhtml += '[tr]';
 			
-			for(j=0;j<2;j++) {
-				preparedhtml += '[td width=20%';
-				if (! sostav[k]) {
-					preparedhtml += ' height=50';
-				}
-				preparedhtml += ']';
-				preparedhtml += ' ';
-				preparedhtml += '[/td]';
-			}
+			preparedhtml += '[td width=20% colspan=2] [/td]';
 			
 			if ( sostav[k] ) {
-					preparedhtml += (pt==2 ? printCard(sostav[k],k) : printClassic(sostav[k],k))
+				switch(pt) {
+					case 3: preparedhtml += printCard2(sostav[k],k);break;
+					case 2: preparedhtml += printCard(sostav[k],k);break;
+					default: printClassic(sostav[k],k)
+				}
 			} else {
 				preparedhtml += '[td width=20% height=50] [/td]';
 			}
 			
-			for(j=0;j<2;j++) {
-				preparedhtml += '[td width=20%]';
-				preparedhtml += ' ';
-				preparedhtml += '[/td]';
-			}
+			preparedhtml += '[td width=20% colspan=2] [/td]';
+
 			preparedhtml += '[/tr]';
 		}
 				
@@ -163,9 +199,13 @@ function fillTextarea(pt) {
 		
 		for(j=12;j<=16;j++) {
 			if ( pids[j] ) {
-				preparedhtml += (pt==2 ? printCard(pids[j],0) : printClassic(pids[j],0))
+				switch(pt) {
+					case 3: preparedhtml += printCard2(pids[j],0);break;
+					case 2: preparedhtml += printCard(pids[j],0);break;
+					default: printClassic(pids[j],0)
+				}
 			} else {
-				preparedhtml += '[td width=20% height=50] [/td]';
+				preparedhtml += '[td width=20%] [/td]';
 			}
 		}
 		
@@ -177,7 +217,7 @@ function fillTextarea(pt) {
 		preparedhtml = preparedhtml.replace(/\[img\]/g,'<img src="').replace(/\[\/img\]/g,'">').replace(/\[/g,'<').replace(/\]/g,'>').replace(/\n/g,'<br>')
 		$('#preview').html(preparedhtml)
 }
-var printtype = 2;
+var printtype = 3
 var data_assoc = [];
 var pids = [];		// id игроков, заявленных в состав
 var players = []; // массив игроков, в котором ключ массива - id игрока, а данные - каждые под своим ключом
@@ -193,9 +233,9 @@ gk_cookie_value = getCookie("gk_uniform");
 if (gk_cookie_value) {
 	goalkeeper_player_img_src = gk_cookie_value;
 }
-	
+
 $().ready(function() {
-		
+
 	$('.back4').html('<table border="0" cellspacing="0" cellpadding="10" width="100%" height="100%"><tr><td valign="top" class="contentframer"></td></tr></table>');
 	$.get('fieldnew.php', {}, function(data){
 		var dataarray = data.split('&');
@@ -236,13 +276,17 @@ $().ready(function() {
 			tmpplayer["passes"] = data_assoc["passes"+i];
 			tmpplayer["form"] = data_assoc["form"+i];
 			tmpplayer["morale"] = data_assoc["morale"+i];
+			tmpplayer["value"] = data_assoc["value"+i].replace(/,/g,'')/1000;
 			var playerid = data_assoc["id" + i];
 			players[playerid] = tmpplayer;
 		}
 		
 		
 		// сбор данных закончен, можно выводить
-		preparedhtml = '<table><tr><td>';
+		preparedhtml = 'Код для форума<br>'
+		preparedhtml += '<textarea rows="10" cols="80" readonly="readonly" id="sostav_na_match"></textarea>';
+
+		preparedhtml += '<br><br><table><tr><td>';
 
 		preparedhtml += '<b>Форма игрока</b><br><table><tr><td>'
 		preparedhtml += '<img src="'+ field_player_img_src +'" alt="" id="fp_uniform_image" />'
@@ -261,14 +305,13 @@ $().ready(function() {
 
 		preparedhtml += '<b>Вид отображения</b><br>'
 		preparedhtml += '<input type="radio" name="prtype" onchange="printtype=1;fillTextarea(printtype)"> классический<br>'
-		preparedhtml += '<input type="radio" name="prtype" onchange="printtype=2;fillTextarea(printtype)" checked> карточка игрока'
+		preparedhtml += '<input type="radio" name="prtype" onchange="printtype=2;fillTextarea(printtype)"> карточка игрока<br>'
+		preparedhtml += '<input type="radio" name="prtype" onchange="printtype=3;fillTextarea(printtype)" checked> карточка игрока(2)'
 		preparedhtml += ''
 
 		preparedhtml += '</td></tr></table>'
 
-		preparedhtml += '<textarea rows="10" cols="80" readonly="readonly" id="sostav_na_match">';
-		preparedhtml += '</textarea>';
-		preparedhtml += '<br><br><table bgcolor=#A3DE8F width=80%><tr><td><span id="preview"></span></td></tr></table>'
+		preparedhtml += '<br><br><table bgcolor=#A3DE8F width=128><tr><td><span id="preview"></span></td></tr></table>'
 		$('.contentframer').html(preparedhtml);	
 		fillTextarea(printtype);
 	});
