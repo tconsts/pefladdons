134203c24130094c46ed2a6ff31e32fdc12a5670
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2010-11-29 18:36:35 +0000

    закончил

--- trunk/team/firefox/team.js
+++ trunk/team/firefox/team.js
@@ -1,20 +1,30 @@
-//1-200т жалкое
-//200-500т бедное
-//500 - 1м среднее
-//1-3м нормальное
-//3-6м благополучное
-//6-15м отличное
-//Больше 15 - богатое
-//Больше 40 - некуда деньги девать
-
 $().ready(function() {
-
 	var txt = 'Фин. положение: '
-
 	$('td.l4:contains('+txt+')').each(function(){
 			var fin = $(this).text().replace(txt,'').replace(/,/g,'').replace('$','')
-			if (fin == 'жалкое') var newtxt =  $(this).text() + ' (1-200т)'
-			if (fin >=1000000 && fin<3000000) var newtxt =  $(this).text()+ ' (нормальное)'
-			$(this).html(newtxt);
+			var newtxt = '';
+			switch (fin){
+				case 'банкрот': newtxt = ' (меньше 0)';break;
+				case 'жалкое': newtxt = ' (1т-200т)';break;
+				case 'бедное': newtxt =  ' (200т-500т)';break;
+				case 'среднее': newtxt =  ' (500т-1м)';break;
+				case 'нормальное': newtxt = ' (1м-3м)';break;
+				case 'благополучное': newtxt = ' (3м-6м)';break;
+				case 'отличное': newtxt =  ' (6м-15м)';break;
+				case 'богатое': newtxt =  ' (15м-40м)';break;
+				case 'некуда деньги девать :-)': newtxt =  ' (>40м)';break;
+				default:
+					if (fin >= 40000000) newtxt = ' (некуда девать)'
+					else if (fin >= 15000000) newtxt = ' (богатое)'
+					else if (fin >= 6000000) newtxt = ' (отличное)'
+					else if (fin >= 3000000) newtxt = ' (благополучное)'
+					else if (fin >= 1000000) newtxt = ' (нормальное)'
+					else if (fin >= 500000) newtxt = ' (среднее)'
+					else if (fin >= 200000) newtxt = ' (бедное)'
+					else if (fin >=0) newtxt = ' (жалкое)'
+					else if (fin < 0) newtxt = ' (банкрот)'
+			}
+			var preparedhtml = 'Фин: '+$(this).text().replace(txt,'').replace(' :-)','')+newtxt.fontsize(1)
+			$(this).html(preparedhtml);
 	});
 });
