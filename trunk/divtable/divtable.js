a002c727e1adaaa8a4340b8d8539deac0ba4aebb
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2010-12-02 12:48:28 +0000

    90% реализация

--- trunk/divtable/divtable.js
+++ trunk/divtable/divtable.js
@@ -1,3 +1,10 @@
+function setCookie(name, value) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + 356); // +1 year
+	if (!name || !value) return false;
+	document.cookie = name + '=' + encodeURIComponent(value) + '; expires='+ exdate.toUTCString() + '; path=/'
+	return true
+}
 function getCookie(name) {
 	    var pattern = "(?:; )?" + name + "=([^;]*);?"
 	    var regexp  = new RegExp(pattern)
@@ -6,18 +13,25 @@ function getCookie(name) {
 	    return false
 }
 
-
 $().ready(function() {
-	var substring = '<u>1</u>'
-	var txt = ''
+	// need develop function of get from url (0 is my table). debug:
+	var tableid = location.href.split('?',2)[1].split('&',4)[3].split('=',2)[1]
 
-	$('td').each(function(i,val){
-		if ($(val).html().indexOf(substring) != -1 && i==12) {
-			$(val).css("background-color", "white");
-			$(val).css("border", "1px solid black");
-			txt += i + ' ' 
-			//alert($(this).html())
-		}
-	});
-	//alert(txt)
+	// need develop function of get(and set) from cockie... debug:
+	var diap = []
+	diap[0]=['18-22:grey','1-2:white']					// my current div
+	diap[43]=['15-16:grey','1-6:yellow','1-2:white']	// Russian PL
+	diap[44]=['18-22:grey','1-2:white']					// Russian PD
+
+	if (diap[tableid]){
+		$('td u').each(function(i,val){
+			var x = $(val).text()
+			for (var j in diap[tableid]) {
+				var d = diap[tableid][j]
+				if (x>= +d.split(':')[0].split('-')[0] && x <= +d.split(':')[0].split('-')[1]) {
+					$(val).parent().parent().parent().css("background-color", d.split(':')[1])
+				}
+			}
+		})
+	}
 });
