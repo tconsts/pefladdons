7077c2d3264c57201b7f7b564a24c2603354ee3f
const_ <const_@ngs.ru>
2010-12-10 18:07:11 +0000

    debug

--- shedule/shedule.js
+++ shedule/shedule.js
@@ -1,5 +1,6 @@
 $().ready(function() {
 	$('td.back4 td').each(function(i,val){
+		var prevdt = ''
 		if ($(val).text().search(/[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]/) == 0) {
 			//$(val).css("border", "1px solid red");
 			var day = ['вск','пнд','втр','срд','чтв','птн','суб']
@@ -8,8 +9,19 @@ $().ready(function() {
 			dt[1] = parseInt((dt[1][0]==0? dt[1][1]:dt[1]))-1
 			dt[2] = parseInt((dt[2][0]==0? dt[2][1]:dt[2]))+2000
 
-			var dateObj = new Date(dt[2],dt[1],dt[0])
-			$(val).html($(val).text()+'&nbsp;' + day[dateObj.getDay()])
+			var curdt = new Date(dt[2],dt[1],dt[0])
+			if (prevdt!=''){
+				var p = (prevdt - curdt)/1000/60/60/24
+				i=1
+				while ( i < p ){
+					var dd = new Date(prevdt - i*60*60*24*1000)
+					var d = day[dd.getDay()]
+					if (d=='пнд' || d=='срд' || d=='птн') $(val).parent().before('<tr bgcolor=white><td colspan=5> <td></tr>')
+					i++
+				}
+				prevdt = curdt
+			}
+			$(val).html($(val).text()+'&nbsp;' + day[curdt.getDay()])
 		}
 	})
 })
