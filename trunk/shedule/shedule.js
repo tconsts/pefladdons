0eec605f03584614ce6e3cebeccd8938ebebe975
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2010-12-12 17:58:16 +0000

    add today

--- trunk/shedule/shedule.js
+++ trunk/shedule/shedule.js
@@ -1,9 +1,10 @@
 $().ready(function() {
+	var day = ['вск','пнд','втр','срд','чтв','птн','суб']
 	var prevdt = ''
+	var today = new Date()
 	$('td.back4 td').each(function(i,val){
 		if ($(val).text().search(/[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]/) == 0) {
 			$(val).parent().attr('bgcolor','#a3de8f')
-			var day = ['вск','пнд','втр','срд','чтв','птн','суб']
 			var dt = $(val).text().split('.')
 			dt[0] = parseInt((dt[0][0]==0? dt[0][1]:dt[0]))
 			dt[1] = parseInt((dt[1][0]==0? dt[1][1]:dt[1]))-1
@@ -20,8 +21,9 @@ $().ready(function() {
 						var str = (dd.getDate()<10 ? '0' : '' ) + dd.getDate() + '.'
 						str += (dd.getMonth()<9 ? '0' : '') + (dd.getMonth()+1) + '.'
 						str += (dd.getFullYear()-2000) + '&nbsp;' + d
+						if (curdt == today) str.bold()
 
-						$(val).parent().before('<tr><td></td><td>'+str.fontcolor('#888A85')+'</td><td></td><td></td><td></td></tr>')
+						$(val).parent().before('<tr><td></td><td>'+str.fontcolor('#888A85')+'</td><td colspan=3></td></tr>')
 					}
 					i++
 				}
@@ -30,4 +32,27 @@ $().ready(function() {
 			prevdt = curdt
 		}
 	})
+	if( today!=prevdt ) {
+		var td = (today.getDate()<10 ? '0' : '' ) + today.getDate() + '.'
+		td += (today.getMonth()<9 ? '0' : '') + (today.getMonth()+1) + '.'
+		td += (today.getFullYear()-2000) + '&nbsp;' + day[today.getDay()]
+		$('td.back4 td:contains("Предстоящие игры") table:first').each(function(){
+			var p = (prevdt - today)/1000/60/60/24
+			var i=1
+			while ( i < p ){
+				var dd = new Date(prevdt - i*60*60*24*1000)
+				var d = day[dd.getDay()]
+				if (d=='пнд' || d=='срд' || d=='птн') {
+					var str = (dd.getDate()<10 ? '0' : '' ) + dd.getDate() + '.'
+					str += (dd.getMonth()<9 ? '0' : '') + (dd.getMonth()+1) + '.'
+					str += (dd.getFullYear()-2000) + '&nbsp;' + d
+					if (curdt == today) str.bold()
+						$(this).append('<tr><td></td><td>'+str.fontcolor('#888A85')+'</td><td colspan=3></td></tr>')
+					}
+				i++
+			}
+			$(this).append('<tr><td></td><td height=25><b>' + td.fontcolor('#888A85') + '</b></td><td colspan=3></td>')
+		})
+
+	}
 })
