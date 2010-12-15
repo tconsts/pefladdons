ad211ca38b278829293f77e48e514d4dfd7f47c4
vtapcov <vtapcov@gmail.com>
2010-12-15 21:58:33 +0000

    Добавил фильтры

--- shedule/shedule.js
+++ shedule/shedule.js
@@ -59,7 +59,11 @@ $().ready(function() {
 		$('td.now').css("border", "1px solid #a3de8f");
 	}
 	
-	function filter(criteria){
+	$('.back4 p:last').html('<a href="#" onclick="filter(\'Чемпионат\'); return false;">Чемпионат</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Кубок\'); return false;">Кубок</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Лига Европы\'); return false;">Лига Европы</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Товарищеский\'); return false;">Товарищеские</a><t/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="$(\'.back4 tr\').fadeIn(); return false;">Все</a><t/>')
+	
+})
+
+function filter(criteria){
 		$('.back4 tr').each(function(index,value){
 			if ($(this).html().indexOf('Матч')<0 && $(this).html().indexOf('now')<0){
 			    if($(this).html().indexOf(criteria)<0){
@@ -69,8 +73,4 @@ $().ready(function() {
 			    }
 			}
 		})
-	}
-
-	$('.back4 p:last').html('<a href="#" onclick="filter(\'Чемпионат\'); return false;">Чемпионат</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Кубок\'); return false;">Кубок</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Лига Европы\'); return false;">Лига Европы</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="filter(\'Товарищеский\'); return false;">Товарищеские</a><t/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onclick="$(\'.back4 tr\').fadeIn(); return false;">Все</a><t/>')
-	
-})
+	};
