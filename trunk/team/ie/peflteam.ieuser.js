d4dee34f1f1fdbf9a1b0e90273f8b693247d7489
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-03-19 17:55:53 +0000

    test ie support

---
+++ trunk/team/ie/peflteam.ieuser.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           peflteam
+// @namespace      pefl
+// @description    roster team page modification (PEFL.ru and .net)
+// @include        http://www.pefl.ru/plug.php?p=refl&t=k&j=*
+// @include        http://pefl.ru/plug.php?p=refl&t=k&j=*
+// @include        http://www.pefl.net/plug.php?p=refl&t=k&j=*
+// @include        http://pefl.net/plug.php?p=refl&t=k&j=*
+// @version			1.2
+// ==/UserScript==
+
+var headID = document.getElementsByTagName("head")[0];         
+var newScript2 = document.createElement('script');
+newScript2.type = 'text/javascript';
+newScript2.src = 'http://pefladdons.googlecode.com/svn/trunk/team/ie/team.js';
+headID.appendChild(newScript2);
