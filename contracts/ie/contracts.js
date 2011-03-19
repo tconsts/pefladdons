f556438f3fa2fae2c64143c0a2f4c03925f2b834
const_ <const_@ngs.ru>
2011-03-19 16:54:54 +0000

    test ie support

---
+++ contracts/ie/contracts.js
@@ -0,0 +1,18 @@
+(function(){
+	var szp=0
+	$('td.back4 td').each(function(i,val){
+		
+		if ($(val).html().indexOf('$') != -1 && !isNaN(+$(val).html().replace('$',''))){
+			szp += +$(val).html().replace('$','')
+		}
+	});
+	var txt='Сумма зарплат:';
+	var newtxt = '<hr><table width=100%><tr><td width=5%></td><td width=30%></td><td width=10% ALIGN=right  bgcolor=#a3de8f><b>'+String((szp/1000).toFixed(3)).replace('.',',')+'$</b></td><td colspan=2><i>(за каждого школьника еще по +100$)</i></td></tr></table>'+txt;
+	
+	$('td.back4').each(function(){
+		if ($(this).html().indexOf(txt) != -1){
+			var newbody = $(this).html().replace(txt,newtxt);
+			$(this).html(newbody);
+		}
+	});
+})();
