d7c76ffa2f49aa454b2f061705d8aae1aed0127c
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-03-19 17:31:31 +0000

    test ie support

---
+++ trunk/player/ie/peflplayer.ieuser.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name           peflplayer
+// @namespace      pefl
+// @description    modification player page (PEFL.ru and .net) and school boys
+// @include        http://www.pefl.ru/plug.php?p=refl&t=p*
+// @include        http://www.pefl.ru/plug.php?p=refl&t=yp*
+// @include        http://pefl.ru/plug.php?p=refl&t=p*
+// @include        http://pefl.ru/plug.php?p=refl&t=yp*
+// @include        http://www.pefl.net/plug.php?p=refl&t=p*
+// @include        http://www.pefl.net/plug.php?p=refl&t=yp*
+// @include        http://pefl.net/plug.php?p=refl&t=p*
+// @include        http://pefl.net/plug.php?p=refl&t=yp*
+
+// ==/UserScript==
+
+var headID = document.getElementsByTagName("head")[0];         
+var newScript2 = document.createElement('script');
+newScript2.type = 'text/javascript';
+newScript2.src = 'http://pefladdons.googlecode.com/svn/trunk/player/ie/player.js';
+headID.appendChild(newScript2);
