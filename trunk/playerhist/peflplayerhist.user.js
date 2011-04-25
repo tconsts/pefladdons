60d82f7278d5f85625e901f8f148a28a22457ca3
termernator@gmail.com <termernator@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-04-25 12:08:01 +0000

    Add "Total" row to player history.

---
+++ trunk/playerhist/peflplayerhist.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name           pefl_playerhist
+// @namespace      pefl
+// @include        http://pefl.ru/hist.php?id=*&t=p
+// ==/UserScript==
+
+
+function replaceOneChar(s,c,n){
+var re = new RegExp('^(.{'+ --n +'}).(.*)$','');
+return s.replace(re,'$1'+c+'$2');
+};
+
+var tbody = document.getElementsByTagName('tbody').item(0); 
+//tbody.deleteRow(0);
+//var rows = tbody.getElementsByTagName('tr'); 
+//rows[0].parentNode.removeChild(rows[0]);    
+var sum = -1; 
+var goals = 0;
+var passes = 0;
+var IM = 0;
+var CP = 0;
+var clubs = 0;
+var i;
+for (i=1; i<tbody.rows.length; i++) 
+{ 
+	//var cols =  rows[i].getElementsByTagName('td');
+	var cols =  tbody.rows[i].cells;
+ 	if (cols[1].innerHTML == '<b>Клуб</b>') 	
+	{
+ 		sum = 0; 		continue; 	
+	} 
+	if (sum < 0) 		
+		continue; 
+	
+	
+	sum += parseInt(cols[2].innerHTML);
+	goals += parseInt(cols[3].innerHTML);
+	passes += parseInt(cols[4].innerHTML);
+	IM += parseInt(cols[5].innerHTML);
+	var sCP = replaceOneChar(cols[6].innerHTML,'.',2);
+    var fCP = parseFloat(sCP);
+    if (fCP > 1)
+    {
+        clubs++;
+        CP += fCP;
+    }
+}
+
+var resRow = tbody.insertRow(-1);
+for( i = 0; i < tbody.rows[0].cells.length; i++)
+{
+	var newCell = resRow.insertCell(-1);	
+	newCell.innerHTML = "<b></b>";
+	newCell.className = "back2";
+}
+
+resRow.cells[0].innerHTML = "<b>Итого</b>";
+resRow.cells[2].innerHTML = "<b>"+sum+"</b>";
+resRow.cells[3].innerHTML = "<b>"+goals+"</b>";
+resRow.cells[4].innerHTML = "<b>"+passes+"</b>";
+resRow.cells[5].innerHTML = "<b>"+IM+"</b>";
+resRow.cells[6].innerHTML = "<b>"+(CP/clubs).toFixed(2)+"</b>";
