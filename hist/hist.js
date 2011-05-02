84be3d986324fa3ab507aa808cef68f2ed6ed447
termernator <termernator@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-05-02 21:22:33 +0000

    0

--- hist/hist.js
+++ hist/hist.js
@@ -11,6 +11,17 @@ var re = new RegExp('^(.{'+ --n +'}).(.*)$','');
 return s.replace(re,'$1'+c+'$2');
 };
 
+// Array.push() - Add an element to the end of an array, return the new length
+if( typeof Array.prototype.push==='undefined' ) {
+ Array.prototype.push = function() {
+  for( var i = 0, b = this.length, a = arguments, l = a.length; i<l; i++ ) {
+   this[b+i] = a[i];
+  }
+  return this.length;
+ };
+}
+
+
 var type = UrlValue('t')
 	// c = club
 	// cw = club winners
@@ -18,42 +29,63 @@ var type = UrlValue('t')
 	// p = player
 
 if (type == 'p') {
+	function PlayerStat()
+	{
+		this.nGames = 0;
+		this.nGoals = 0;
+		this.nAssists = 0;
+		this.nMVP = 0;
+		this.nAvgMark = 0;
+	}
+
 	var tbody = document.getElementsByTagName('tbody').item(0); 
-	var sum = -1; 
-	var goals = 0;
-	var passes = 0;
-	var IM = 0;
-	var CP = 0;
-	var clubs = 0;
-	var i;
-	var text = ''
-	for (i=1; i<tbody.rows.length; i++) 
+	var aPlayerStats = new Array();
+	
+	var bCanParseStats = false;
+	for (var i=1; i<tbody.rows.length; i++) 
 	{ 
 		var cols =  tbody.rows[i].cells;
 
-	 	if (cols[1].innerHTML.indexOf('Сборная') >= 0 ||
-	        cols[1].innerHTML.indexOf('Клуб') >= 0) 	
+		if (cols[1].innerHTML.indexOf('Сборная') >= 0 ||
+			cols[1].innerHTML.indexOf('Клуб') >= 0) 	
 		{
-	 		if (sum < 0) 
-	            sum = 0; 		
-	        continue;
+			bCanParseStats = true;
+			continue;
 		} 
-	
-	    if (sum < 0) 		
-			continue; 
-	
-		sum += parseInt(cols[2].innerHTML);
-		goals += parseInt(cols[3].innerHTML);
-		passes += parseInt(cols[4].innerHTML);
-		IM += parseInt(cols[5].innerHTML);
-		var sCP = replaceOneChar(cols[6].innerHTML,'.',2);
-	    var fCP = parseFloat(sCP);
-	    if (fCP > 1)
-	    {
-	        clubs++;
-	        CP += fCP;
-	    }
+
+		if (!bCanParseStats) 		
+			continue; // не добрались еще до клубов в статистике
+
+		var iCurrentClubIndex = aPlayerStats.length - 1;
+
+		iCurrentClubIndex = aPlayerStats.push(new PlayerStat()) - 1; 
+		var ps = aPlayerStats[iCurrentClubIndex];
+		ps.nGames = parseInt(cols[2].innerHTML);
+		ps.nGoals = parseInt(cols[3].innerHTML);
+		ps.nAssists = parseInt(cols[4].innerHTML);
+		ps.nMVP = parseInt(cols[5].innerHTML);
+		var fCP = parseFloat(replaceOneChar(cols[6].innerHTML,'.',2));
+		if (fCP > 1)
+			ps.nAvgMark = fCP*ps.nGames;
 	}
+
+	var nTotalGames = 0;
+	var nTotalGoals = 0;
+	var nTotalAssists = 0; 
+	var nTotalMVP = 0;
+	var nAMSum = 0;
+	var nAMCount = 0; 
+
+	for( var tIdx = 0; tIdx < aPlayerStats.length; tIdx++ )
+	{
+		nTotalGames += aPlayerStats[tIdx].nGames;
+		nTotalGoals += aPlayerStats[tIdx].nGoals;
+		nTotalAssists += aPlayerStats[tIdx].nAssists;
+		nTotalMVP += aPlayerStats[tIdx].nMVP;
+		if (aPlayerStats[tIdx].nAvgMark > 1) 
+			nAMSum += aPlayerStats[tIdx].nAvgMark;
+	}
+	var nAvgMark = nAMSum / nTotalGames;
 
 	var resRow = tbody.insertRow(-1);
 	resRow = tbody.insertRow(-1);
@@ -61,15 +93,18 @@ if (type == 'p') {
 	{
 		var newCell = resRow.insertCell(-1);	
 		newCell.innerHTML = "<b></b>";
-	    newCell.bgColor = "#88C274";
+		newCell.bgColor = "#88C274";
 	}
 
 	resRow.cells[0].innerHTML = "<b>Итого</b>";
-	resRow.cells[2].innerHTML = "<b>"+sum+"</b>";
-	resRow.cells[3].innerHTML = "<b>"+goals+"</b>";
-	resRow.cells[4].innerHTML = "<b>"+passes+"</b>";
-	resRow.cells[5].innerHTML = "<b>"+IM+"</b>";
-	resRow.cells[6].innerHTML = "<b>"+(CP/clubs).toFixed(2)+"</b>";
+	resRow.cells[2].innerHTML = "<b>"+nTotalGames+"</b>";
+	resRow.cells[3].innerHTML = "<b>"+nTotalGoals+"</b>";
+	resRow.cells[4].innerHTML = "<b>"+nTotalAssists+"</b>";
+	resRow.cells[5].innerHTML = "<b>"+nTotalMVP+"</b>";
+	resRow.cells[6].innerHTML = "<b>"+(nAvgMark).toFixed(2)+"</b>";
+
 } else {
+
+
 
 }
