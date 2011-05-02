bd308ed9981c1e78257a4378b6567403bc508359
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-05-02 16:43:19 +0000

    for release hist script

--- trunk/hist/ie/peflhist.ieuser.js
+++ trunk/hist/ie/peflhist.ieuser.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name           peflplayerhist
 // @namespace      pefl
+// @description    history page modification (PEFL)
 // @include        http://pefl.ru/hist.php?id=*&t=p
 // @include        http://www.pefl.ru/hist.php?id=*&t=p
 // @include        http://pefl.net/hist.php?id=*&t=p
@@ -12,5 +13,5 @@
 var headID = document.getElementsByTagName("head")[0];         
 var newScript = document.createElement('script');
 newScript.type = 'text/javascript';
-newScript.src = 'http://pefladdons.googlecode.com/svn/trunk/playerhist/playerhist.js';
+newScript.src = 'http://pefladdons.googlecode.com/svn/trunk/hist/ie/hist.js';
 headID.appendChild(newScript);
---
+++ trunk/hist/peflhist.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name           peflplayerhist
+// @namespace      pefl
+// @description    history page modification (PEFL)
+// @include        http://pefl.ru/hist.php?id=*&t=p
+// @include        http://www.pefl.ru/hist.php?id=*&t=p
+// @include        http://pefl.net/hist.php?id=*&t=p
+// @include        http://www.pefl.net/hist.php?id=*&t=p
+// @include        http://pefl.org/hist.php?id=*&t=p
+// @include        http://www.pefl.org/hist.php?id=*&t=p
+// ==/UserScript==
+
+var headID = document.getElementsByTagName("head")[0];         
+var newScript = document.createElement('script');
+newScript.type = 'text/javascript';
+newScript.src = 'http://pefladdons.googlecode.com/svn/trunk/hist/hist.js';
+headID.appendChild(newScript);
