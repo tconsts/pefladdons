931411086f6dd60643658bf4fa8c0fc705b605d7
const_@ngs.ru <const_@ngs.ru@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2011-05-02 16:58:38 +0000

    set version

--- trunk/hist/opera/peflhist.js
+++ trunk/hist/opera/peflhist.js
@@ -8,6 +8,7 @@
 // @include        http://www.pefl.net/hist.php?id=*&t=*
 // @include        http://pefl.org/hist.php?id=*&t=*
 // @include        http://www.pefl.org/hist.php?id=*&t=*
+// @version			1.0
 // ==/UserScript==
 
 function UrlValue(key,url){
