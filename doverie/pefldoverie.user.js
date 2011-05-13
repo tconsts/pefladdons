79c404a15d91b54fc4952960cbb07678f11ac9da
const_ <const_@ngs.ru>
2011-05-13 14:32:25 +0000

    new script doverie

---
+++ doverie/pefldoverie.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           pefldov
+// @namespace      pefl
+// @description    modification credit page
+// @include        http://www.pefl.ru/plug.php?p=refl&t=dov&*
+// @include        http://pefl.ru/plug.php?p=refl&t=dov&*
+// @include        http://www.pefl.net/plug.php?p=refl&t=dov&*
+// @include        http://pefl.net/plug.php?p=refl&t=dov&*
+// @include        http://www.pefl.org/plug.php?p=refl&t=dov&*
+// @include        http://pefl.org/plug.php?p=refl&t=dov&*
+// @version			1.0
+// ==/UserScript==
+
+var headID = document.getElementsByTagName("head")[0];         
+var newScript2 = document.createElement('script');
+newScript2.type = 'text/javascript';
+newScript2.src = 'http://pefladdons.googlecode.com/svn/trunk/doverie/doverie.js';
+headID.appendChild(newScript2);
