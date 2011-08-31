144e04c3116dbf470de3305467e6384659b97e7f
const_ <const_@ngs.ru>
2011-08-31 11:32:31 +0000

    add Web SQL

---
+++ team/peflteam_dev.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           peflteam
+// @namespace      pefl
+// @description    roster team page modification
+// @include        http://*pefl.*/*&t=k&j=*
+// @version        2.0
+// ==/UserScript==
+
+var headID = document.getElementsByTagName("head")[0];         
+var newScript2 = document.createElement('script');
+newScript2.type = 'text/javascript';
+newScript2.src = 'http://pefladdons.googlecode.com/svn/trunk/team/team.js';
+headID.appendChild(newScript2);
