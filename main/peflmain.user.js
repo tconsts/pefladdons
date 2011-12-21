28ce84eb61e28c4f7ecf16660e731a05214efdcc
tconsts <tconsts@gmail.com>
2011-12-21 09:59:29 +0000

    add mail to main

--- main/peflmain.user.js
+++ main/peflmain.user.js
@@ -13,3 +13,13 @@ var newScript1 = document.createElement('script');
 newScript1.type = 'text/javascript';
 newScript1.src = 'http://pefladdons.googlecode.com/svn/trunk/main/main.js';
 headID.appendChild(newScript1);
+
+var url1 = location.pathname.substring(1)
+var url2 = location.search.substring(1)
+
+if(url1=='pm.php'){
+	var newScriptMail = document.createElement('script');
+	newScriptMail.type = 'text/javascript';
+	newScriptMail.src = 'http://pefladdons.googlecode.com/svn/trunk/mail/mail.js';
+	headID.appendChild(newScriptMail);
+}
