26287474ecd8c15c2427649508c1f449d45a8c4d
tconsts@gmail.com <tconsts@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2012-02-03 11:05:57 +0000

    dev

--- trunk/peflforum.js
+++ trunk/peflforum.js
@@ -7,9 +7,33 @@
 // ==/UserScript==
 
 $().ready(function() {
+
+	var forumflags = '0:0:1:0:0:0'.split(':')
+	if(localStorage.forum!=undefined && localStorage.forum!=null) forumflags = localStorage.forum.split(':')
+
+	var txt = ''
 	$('td.back4 tr:gt(2) td[width=128]').each(function(){
-		var txt = $(this).html().split('<br>')
-		$(this).html(txt[0]+'<br>'+txt[1]+'<br>'+txt[5].replace('Сообщений',''))
-	})
-	$('span.text1xs').remove()
+
+		// span.text2b a(имя) img(человечек) img($, может не быть)
+		if(forumflags[0]==1) $(this).find('span.text2b img').remove()
+
+		// a(клуб)
+		if(forumflags[1]==1) $(this).find('a[href^="plug.php?p=refl&t=k&j="]').remove()
+
+		// span.text1s a(Пользователь) br
+		if(forumflags[2]==1) $(this).find('span.text1s:has(a:contains(Пользователь))').remove()
+
+		// img(грузовички)
+//		if(forumflags[1]==1) //$(this).find()
+
+		// сообщений кл-во
+
+		// репутация a(-1) a(0) a(+1)
+		// репа[] img
+		// " "
+		// img(аватарка, может не быть)
+		// " "
+		// span.text1xs "откуда" img(flag) <br> профессия <hr> img a(сборная) <hr> регалии
+	});
+
 }, false);
