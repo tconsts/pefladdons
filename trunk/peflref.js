c80dcb2d9bcd5d386f99299a48f33cb99c2fc47a
tconsts@gmail.com <tconsts@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2012-02-06 17:00:13 +0000

    add ref

---
+++ trunk/peflref.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           peflref
+// @namespace      pefl
+// @description    referee page modification
+// @include        http://*pefl.*/*&t=ref&*
+// ==/UserScript==
+
+$().ready(function() {
+	
+});
