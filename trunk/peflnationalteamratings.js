601a796588f65560b423cf84ac2d84df27aea4fa
lifeseo@gmail.com <lifeseo@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2012-11-06 19:00:08 +0000

    Добавление опции кода для форума для рейтинга сборных в настройки

--- trunk/peflnationalteamratings.js
+++ trunk/peflnationalteamratings.js
@@ -2,7 +2,7 @@
 // @name           peflnationalteamratings
 // @namespace      pefl
 // @description    national teams ratings page modification
-// @include        http://*pefl.*/plug.php?p=rating&t=sb2&n=*
+// @include        http://*pefl.*/plug.php?p=rating&t=sb2&j=*
 // @version	       1.0
 // ==/UserScript==
 
