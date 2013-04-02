eeb2a60155aa2787c3422e537b9b64728ebaacfd
tconsts@gmail.com <tconsts@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2013-04-02 05:41:29 +0000

    запоминаем градацию школы

--- trunk/peflreitschool.js
+++ trunk/peflreitschool.js
@@ -6,8 +6,20 @@
 // @version	       2.0
 // ==/UserScript==
 
+
+function UrlValue(key,url){
+	var pf = (url ? url.split('?',2)[1] : location.search.substring(1)).split('&');
+	for (n in pf){if (pf[n].split('=')[0] == key) return pf[n].split('=')[1]};
+	return false;
+}
+function debug(text) {if(deb) {debnum++;$('td.back4').append(debnum+'&nbsp;\''+text+'\'<br>');}}
+
 $().ready(function() {
-/**
-	временно отключен
-**/
+
+	//рейтинг по репутации
+	if(UrlValue('n')==1){
+		if($('tr.back6').length>0) localStorage.schreitrep = parseInt($('tr.back6').attr('id'))
+		else delete localStorage.schreitrep
+	}
+
 }, false);
