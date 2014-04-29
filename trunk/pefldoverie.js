ee0d7aee34a3b822cdbb0654d0f1d34bd614e351
tconsts@gmail.com <tconsts@gmail.com@426b52c1-5bf6-6d88-c1aa-cf4cada07c41>
2014-04-29 18:05:26 +0000

    add code for forum

--- trunk/pefldoverie.js
+++ trunk/pefldoverie.js
@@ -38,16 +38,26 @@ $().ready(function() {
 	mdov2[1] = x[x.length-1].split('.')[0]
 
     var text = ''
-	text += '<hr><table align=center width=100%><tr><td><table bgcolor=A3DE8F width=100%>'
+	text += '<hr><table align=center width=100% idd1><tr><td><table bgcolor=A3DE8F width=100% id=d2>'
 	text += '<tr><th>Оценка управления клубом</th></tr>'
 	for (i in dov1)	text += '<tr bgcolor=C9F8B7><td ' + (mdov1==dov1[i] ? ' bgcolor=white': '') + '>' + dov1[i] + '.</td></tr>'
 	text += '</table></td>'
 
-	text += '<td valign=top><table bgcolor=A3DE8F width=100%>'
+	text += '<td valign=top><table bgcolor=A3DE8F width=100% id=d3>'
 	text += '<tr><th>' + mdov2[0] + '</th></tr>'
 	for (i in dov2)	text += '<tr bgcolor=C9F8B7><td align=center ' + (mdov2[1] == dov2[i] ? ' bgcolor=white': '') + '>' + dov2[i] + '</td></tr>'
 	text += '</table></tr></td>'
 
 	$('td.back4').append(text)
+
+	var tables = [];
+	$('td.back4 table table').each(function(i,val){
+		if($(val).attr('id')==undefined || $(val).attr('id')=='') $(val).attr('id','x'+i);
+		tables.push($(val).attr('id'));
+	})
+	var text = '</script><script type="text/javascript" src="js/fcode2.js"></script>';
+	text+='<div align=right><a href="javascript:void(ShowCode([],\''+tables.join(',')+'\',\'forumcode\'))">код для форума</a></div>';
+	$('td.back4 table table:first').before(text);
+
 }, false);
 //})();
